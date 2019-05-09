a60a3131701557bda3240bd32ce29a86536c1d36
Christian Blom <christian.blom@chasacademy.se>
2019-05-09 11:49:50 +0200

    Connected the Landing component to redux and tried adding a useEffect hook to redirect to dashboard when auth.isAuthenticated is true, not working so far.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,46 +1,67 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Landing = () => (
-  <div style={{ height: "75vh" }} className="container valign-wrapper">
-    <div className="row">
-      <div className="col s12 center-align">
-        <h4>
-          <b>Build</b> a login/auth app with the{" "}
-          <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-          scratch
-        </h4>
-        <p className="flow-text grey-text text-darken-1">TEXT</p>
-        <br />
-        <div className="col s6">
-          <Link
-            to="/register"
-            style={{
-              width: "140px",
-              borderRadius: "3px",
-              letterSpacing: "1.5px"
-            }}
-            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-          >
-            Register
-          </Link>
-        </div>
-        <div className="col s6">
-          <Link
-            to="/login"
-            style={{
-              width: "140px",
-              borderRadius: "3px",
-              letterSpacing: "1.5px"
-            }}
-            className="btn btn-large btn-flat waves-effect white black-text"
-          >
-            Log In
-          </Link>
+function Landing(props) {
+  const { auth, history } = props;
+  
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      history.push("/dashboard");
+    }
+  });
+
+  return (
+    <div style={{ height: "75vh" }} className="container valign-wrapper">
+      <div className="row">
+        <div className="col s12 center-align">
+          <h4>
+            <b>Build</b> a login/auth app with the{" "}
+            <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
+            scratch
+          </h4>
+          <p className="flow-text grey-text text-darken-1">TEXT</p>
+          <br />
+          <div className="col s6">
+            <Link
+              to="/register"
+              style={{
+                width: "140px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px"
+              }}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Register
+            </Link>
+          </div>
+          <div className="col s6">
+            <Link
+              to="/login"
+              style={{
+                width: "140px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px"
+              }}
+              className="btn btn-large btn-flat waves-effect white black-text"
+            >
+              Log In
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
-export default Landing;
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(withRouter(Landing));
