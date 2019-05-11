9f14a8a84c9937f10db9ab698422a8d8b6912bf8
Christian Blom <christian.blom@chasacademy.se>
2019-05-11 14:33:12 +0200

    Tried enabling cors default, allowing everything.

--- server/src/server.js
+++ server/src/server.js
@@ -30,6 +30,8 @@ app.use(
   })
 );
 
+app.use(cors());
+
 app.use(bodyParser.json());
 
 mongoose.connect(db, { useNewUrlParser: true });
@@ -55,13 +57,4 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
   console.log(`Server up and running on port ${port} test!`)
-);
-
-app.use(
-  cors({
-    origin: process.env.ALLOW_ORIGIN,
-    credentials: true,
-    allowedHeaders: "X-Requested-With, Content-Type, Authorization",
-    methods: "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  })
 );
