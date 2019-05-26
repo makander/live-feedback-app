d6e6f26a28aee8a2f4ec6c7a0ced96ab75c5191b
Christian Blom <christian.blom@chasacademy.se>
2019-05-26 21:28:00 +0200

    Added some verification tests and tried to add a conditional variable with either the express server or the localhost address to supertest depending on if the tests are run locally or in the pipeline.

--- server/src/tests/basic.test.js
+++ server/src/tests/basic.test.js
@@ -8,87 +8,123 @@ import mongoose from "mongoose";
 
 require("should-http");
 
-process.env.NODE_ENV = "test";
-process.env.API_BASE = "/api";
-
 const hostUrl = "http://localhost:5000";
 const server = require("../server");
-const request = require("supertest")(server);
+const request = require("supertest");
 const should = require("should");
-const assert = require("assert");
 
+const testServer = process.env.NODE_ENV === "testing" ? server : hostUrl;
 const newUser = {
-  "name": "Christian Test",
-  "email": "christian-test@anneblom.se",
-  "password": "secret",
-  "password2": "secret"
+  name: "Christian Test",
+  email: "christian-test@anneblom.se",
+  password: "secret",
+  password2: "secret"
 };
 
-describe("Database tests", function () {
-  before(function (done) {
-
+describe("Database tests", function() {
+  before(function(done) {
     function clearDB() {
-      const promises = [
-        User.deleteMany({}).exec(),
-    ];
+      const promises = [User.deleteMany({}).exec()];
 
-      Promise.all(promises)
-        .then(() => {
-          done();
-        })
+      Promise.all(promises).then(() => {
+        done();
+      });
     }
 
     if (mongoose.connection.readyState === 0) {
-      mongoose.connect(process.env.MONGO_TEST_CONNECTION, { useNewUrlParser: true }, function (err) {
-        if (err) {
-          throw err;
+      mongoose.connect(
+        process.env.MONGO_TEST_CONNECTION,
+        { useNewUrlParser: true },
+        function(err) {
+          if (err) {
+            throw err;
+          }
+          return clearDB();
         }
-        return clearDB();
-      });
+      );
     } else {
       return clearDB();
     }
   });
 
   describe("User tests", function() {
-    it("should return missing name", function (done) {
-      request.post(`/api/users/register`)
+    it("should return missing name", function(done) {
+      request(testServer)
+        .post(`/api/users/register`)
         .send({
           email: newUser.email,
           password: newUser.password,
           password2: newUser.password2
         })
         .expect(400)
-        .end(function (err, res) {
+        .end(function(err, res) {
           if (err) return done(err);
           should.exist(res.body);
           res.body.name.should.equal("Name field is required");
           done();
-        })
-    })
-
-    it("should create user", function (done) {
-      request.post(`/api/users/register`)
-        .send(newUser)
-        .expect(200)
-        .end(function (err, res) {
-          if (err) return done(err);
-          should.exist(res.body);
-          res.body.ok.should.equal(true);
-          done();
-        })
+        });
     });
 
-    it("should return token on login", function (done) {
-      function tokenIsSet(res) {
-        if (!('token' in res.body.data)) throw new Error("missing JWT token");
-      }
+    it("should return missing email", function(done) {
+      request(testServer)
+        .post(`/api/users/register`)
+        .send({
+          name: newUser.name,
+          password: newUser.password,
+          password2: newUser.password2
+        })
+        .expect(400)
+        .end(function(err, res) {
+          if (err) return done(err);
+          should.exist(res.body);
+          res.body.email.should.equal("Email field is required");
+          done();
+        });
+    });
 
-      request.post("/api/users/login")
-        .send({ email: newUser.email, password: newUser.password })
+    it("should return passwords must match", function(done) {
+      request(testServer)
+        .post(`/api/users/register`)
+        .send({
+          name: newUser.name,
+          email: newUser.email,
+          password: newUser.password
+        })
+        .expect(400)
+        .end(function(err, res) {
+          if (err) return done(err);
+          should.exist(res.body);
+          res.body.password2.should.equal("Passwords must match");
+          done();
+        });
+    });
+
+    it("should return password must be longer than 6 characters", function(done) {
+      request(testServer)
+        .post(`/api/users/register`)
+        .send({
+          name: newUser.name,
+          email: newUser.email,
+          password: "test",
+          password2: "test"
+        })
+        .expect(400)
+        .end(function(err, res) {
+          if (err) return done(err);
+          should.exist(res.body);
+          res.body.password.should.equal(
+            "Password must be at least 6 characters"
+          );
+          done();
+        });
+    });
+
+    it("should create user", function(done) {
+      request(testServer)
+        .post(`/api/users/register`)
+        .send(newUser)
         .expect(200)
-        .expect(tokenIsSet)
-        .end(function (err, res) {
+        .end(function(err, res) {
           if (err) return done(err);
           should.exist(res.body);
           res.body.ok.should.equal(true);
@@ -96,11 +132,30 @@ describe("Database tests", function () {
         });
     });
 
-    it("should fail when logging in with incorrect credentials", function (done) {
-      request.post("/api/users/login")
-        .send({ email: "bla@bla.com", password: "hejhej"})
+    it("should return token on login", function(done) {
+      function tokenIsSet(res) {
+        if (!("token" in res.body.data)) throw new Error("missing JWT token");
+      }
+
+      request(testServer)
+        .post("/api/users/login")
+        .send({ email: newUser.email, password: newUser.password })
+        .expect(200)
+        .expect(tokenIsSet)
+        .end(function(err, res) {
+          if (err) return done(err);
+          should.exist(res.body);
+          res.body.ok.should.equal(true);
+          done();
+        });
+    });
+
+    it("should fail when logging in with incorrect credentials", function(done) {
+      request(testServer)
+        .post("/api/users/login")
+        .send({ email: "bla@bla.com", password: "hejhej" })
         .expect(401)
-        .end(function (err, res) {
+        .end(function(err, res) {
           if (err) return done(err);
           should.exist(res.body);
           res.body.ok.should.equal(false);
@@ -109,9 +164,10 @@ describe("Database tests", function () {
     });
 
     it("should send correct logout message", function(done) {
-      request.get("/api/users/logout")
+      request(testServer)
+        .get("/api/users/logout")
         .expect(200)
-        .end(function (err, res) {
+        .end(function(err, res) {
           if (err) return done(err);
           should.exist(res.body);
           res.body.msg.should.equal("Successfully logged out");
@@ -119,4 +175,4 @@ describe("Database tests", function () {
         });
     });
   });
-})
+});
