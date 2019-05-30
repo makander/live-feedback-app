d7ea078f34ffe7d872e770eef350681d0b46105d
Fred Bellinder <fred.bellinder@chasacademy.se>
2019-05-30 13:44:55 +0200

    :broom: Linter warnings solved

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -6,8 +6,8 @@ import classnames from "classnames";
 import { registerUser } from "../actions/auth";
 
 class Register extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       name: "",
       email: "",
@@ -56,104 +56,104 @@ class Register extends Component {
     const { errors, name, email, password, password2 } = this.state;
 
     return (
-      <div className="d-flex justify-content-center pt-2">
+      <div className="d-flex justify-content-center pt-2 ">
         <div
           className="border border-info px-5 pt-5"
-          style={{ marginBottom: 8 + "rem", width: "75vh" }}
+          style={{ marginBottom: "8rem", width: "90vh" }}
         >
           <div>
             <h1 className="text-center">Register</h1>
           </div>
           <form noValidate onSubmit={this.onSubmit}>
-            <div className="form-group row" style={{ marginTop: 3 + "rem" }}>
-              <label htmlFor="inputName" className="col-sm-3 col-form-label">
-                Name
-              </label>
-              <div className="col-sm-8">
-                <input
-                  type="name"
-                  id="inputEmail3"
-                  placeholder="Enter name"
-                  onChange={this.onChange}
-                  value={name}
-                  error={errors.name}
+            <div className="form-group row" style={{ marginTop: "3rem" }}>
+              <div className="col-sm-12">
+                <label
                   id="name"
-                  type="text"
-                  className={classnames("form-control", {
-                    invalid: errors.name
-                  })}
-                />
-                <span className="red-text">{errors.name}</span>
+                  htmlFor="name"
+                  className="col-sm-12 col-form-label"
+                >
+                  Name
+                  <input
+                    type="name"
+                    id="name"
+                    placeholder="Enter name"
+                    onChange={this.onChange}
+                    value={name}
+                    error={errors.name}
+                    className={classnames("form-control", {
+                      invalid: errors.name
+                    })}
+                  />
+                </label>
+                <span className="text-danger">{errors.name}</span>
               </div>
             </div>
 
             <div className="form-group row py-1">
-              <label htmlFor="inputEmail" className="col-sm-3 col-form-label">
-                Email
-              </label>
-              <div className="col-sm-8">
-                <input
-                  placeholder="Enter email"
-                  onChange={this.onChange}
-                  value={email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("form-control", {
-                    invalid: errors.email
-                  })}
-                />
-                <span className="red-text">{errors.email}</span>
+              <div className="col-sm-12">
+                <label
+                  id="inputEmailLabel"
+                  htmlFor="email"
+                  className="col-sm-12 col-form-label"
+                >
+                  Email
+                  <input
+                    placeholder="Enter email"
+                    onChange={this.onChange}
+                    value={email}
+                    error={errors.email}
+                    id="email"
+                    type="email"
+                    className={classnames("form-control", {
+                      invalid: errors.email
+                    })}
+                  />
+                  <span className="text-danger">{errors.email}</span>
+                </label>
               </div>
             </div>
 
             <div className="form-group row py-1">
-              <label
-                htmlFor="inputPassword"
-                className="col-sm-3 col-form-label"
-              >
-                Password
-              </label>
-              <div className="col-sm-8">
-                <input
-                  placeholder="Enter password"
-                  onChange={this.onChange}
-                  value={password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("form-control", {
-                    invalid: errors.password
-                  })}
-                />
-                <span className="red-text">{errors.password}</span>
+              <div className="col-sm-12">
+                <label htmlFor="password" className="col-sm-12 col-form-label">
+                  Password
+                  <input
+                    placeholder="Enter password"
+                    onChange={this.onChange}
+                    value={password}
+                    error={errors.password}
+                    id="password"
+                    type="password"
+                    className={classnames("form-control", {
+                      invalid: errors.password
+                    })}
+                  />
+                  <span className="text-danger">{errors.password}</span>
+                </label>
               </div>
             </div>
 
             <div className="form-group row py-1">
-              <label
-                htmlFor="inputPasswordConfirm"
-                className="col-sm-3 col-form-label"
-              >
-                Confirm Password
-              </label>
-              <div className="col-sm-8">
-                <input
-                  placeholder="Confirm password"
-                  onChange={this.onChange}
-                  value={password2}
-                  error={errors.password2}
-                  id="password2"
-                  type="password"
-                  className={classnames("form-control", {
-                    invalid: errors.password2
-                  })}
-                />
-                <span className="red-text">{errors.password2}</span>
+              <div className="col-sm-12">
+                <label htmlFor="password2" className="col-sm-12 col-form-label">
+                  Confirm Password
+                  <input
+                    placeholder="Confirm password"
+                    onChange={this.onChange}
+                    value={password2}
+                    error={errors.password2}
+                    id="password2"
+                    type="password"
+                    className={classnames("form-control", {
+                      invalid: errors.password2
+                    })}
+                  />
+                  <span className="text-danger">{errors.password2}</span>
+                </label>
               </div>
             </div>
 
-            <div style={{ marginTop: 3 + "rem" }}>
+            <div style={{ marginTop: "3rem" }}>
               <button
                 type="submit"
                 className="btn btn-primary btn-lg btn-block"
@@ -162,7 +162,7 @@ class Register extends Component {
               </button>
             </div>
           </form>
-          <p style={{ marginTop: 3 + "rem" }} className="text-center">
+          <p style={{ marginTop: "3rem" }} className="text-center">
             Already have an account? <Link to="/login">Login</Link>
           </p>
         </div>
