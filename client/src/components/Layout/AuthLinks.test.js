1f42094736a53f637051831586166e86beddcbfe
daniel-salin <daniel.salin@chasacademy.se>
2019-05-31 11:30:34 +0200

    :white_check_mark: Update AutLinks test and add test setup in client

---
+++ client/src/components/Layout/AuthLinks.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { shallow } from "enzyme";
+import configureStore from "redux-mock-store";
+import { AuthLinks } from "./AuthLinks";
+
+const initialState = {};
+const mockStore = configureStore();;
+let store;
+
+describe("Login Component", () => {
+  let wrapper;
+
+  const mockLogout = jest.fn();
+  
+    // pass the mock function as the logout prop
+
+    beforeEach(() => {
+      store = mockStore(initialState);
+      wrapper = shallow(<AuthLinks store={store} logout={mockLogout} />);
+    });
+
+  describe("When the logout button is pressed", () => {
+    it("should call the mock logout function", () => {
+      wrapper.find("a.nav-item").simulate('click', {preventDefault() {}});
+      expect(mockLogout.mock.calls.length).toEqual(1)
+    });
+  });
+});
